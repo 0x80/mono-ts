19a2a1f04f1d4461847ff3befb983112439d0fbb
Thijs Koerselman <thijskoerselman@gmail.com>
2023-11-20 17:13:54 +0100

    Use the "internal packages" strategy

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,3 @@
+export type * from './firebase';
+export type * from './utils/index.js';
+
--- services/api/tsup.config.ts
+++ services/api/tsup.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
-  target: "es2022",
+  target: "node18",
+  clean: true,
   sourcemap: true,
-
+  noExternal: ["@mono/common", "@mono/backend"],
   /**
    * Do not use tsup for generating d.ts files because it can not generate type
    * the definition maps required for go-to-definition to work in our IDE. We
