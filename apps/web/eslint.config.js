9956def2767abf7550b128200fa58d9e61bca083
Thijs Koerselman <thijskoerselman@gmail.com>
2025-01-19 14:01:36 +0100

    Clean up

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -5,18 +5,16 @@ import baseConfig from "@repo/eslint-config/base";
 import tseslint from "typescript-eslint";
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 });
 
 export default tseslint.config(
   ...baseConfig,
-  // js.configs.recommended,
   ...compat.config({
     extends: ["next"],
     settings: {
       next: {
-        rootDir: "apps/web/",
+        rootDir: "apps/web/", // required for monorepos
       },
     },
   }),
@@ -30,16 +28,7 @@ export default tseslint.config(
       },
     },
     rules: {
-      // "@typescript-eslint/no-non-null-assertion": "off", // @TODO enable later
-      // "@typescript-eslint/no-confusing-void-expression": "off", // @TODO enable later
-      // "@typescript-eslint/no-unsafe-assignment": "off", // @TODO enable later
-      // "@typescript-eslint/restrict-template-expressions": "off", // @TODO enable later
-      // "@typescript-eslint/no-deprecated": "off",
       "@typescript-eslint/use-unknown-in-catch-callback-variable": "off",
-      // "@typescript-eslint/no-unused-vars": [
-      //   "warn",
-      //   { argsIgnorePattern: "^_" },
-      // ],
     },
   }
 );
