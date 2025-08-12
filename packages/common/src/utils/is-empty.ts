c4f91e1c0bc287bc380252744ca73e80411006bc
Thijs Koerselman <thijskoerselman@gmail.com>
2025-08-12 12:22:03 +0200

    Fix linting

--- packages/common/src/utils/is-empty.ts
+++ packages/common/src/utils/is-empty.ts
@@ -2,6 +2,6 @@
  * Arrays only. Typically isEmpty does more than that, but we don't need it
  * here.
  */
-export function isEmpty<T>(array: T[]): array is [] {
+export function isEmpty(array: unknown[]): array is [] {
   return array.length === 0;
 }
