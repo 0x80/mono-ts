e9a8fe6892e772f778ea5b57800740025e0fdaf6
Thijs Koerselman <thijskoerselman@gmail.com>
2025-08-12 13:48:45 +0200

    Yep

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -26,6 +26,6 @@ export default [
     },
   },
   {
-    ignores: ["dist/**", "node_modules/**", "isolate/**", "*.{js,ts}"],
+    ignores: ["dist/**", "node_modules/**", "isolate/**", "**/*.test.{js,ts}", "**/*.spec.{js,ts}"],
   },
 ];
---
+++ packages/eslint-config/eslint.config.js
@@ -0,0 +1,7 @@
+import base from "./base.js";
+
+/**
+ * Main ESLint configuration for the eslint-config package.
+ * This file serves as the entry point for ESLint v9+ flat config.
+ */
+export default base;
