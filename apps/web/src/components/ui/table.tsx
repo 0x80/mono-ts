7489d52b88b2a133b9ea0ff481de7e392c817836
Thijs Koerselman <thijskoerselman@gmail.com>
2025-08-12 13:44:33 +0200

    Ok go

--- apps/web/src/components/ui/table.tsx
+++ apps/web/src/components/ui/table.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 
 import { cn } from "~/lib/utils";
 
+/* eslint-disable react/prop-types */
+
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
