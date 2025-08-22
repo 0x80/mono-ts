79dac348139be876bb4c3c99410f7af5ef05736d
brianGammon <bgammon@gmail.com>
2025-08-22 13:25:20 -0400

    Replace ESLint and Prettier with Biome and fix emulator startup (#47)
    * Remove ESLint and Prettier configurations, replacing them with Biome v2.2.0 for unified linting and formatting
    
    * Update Firebase functions runtime from Node.js 20 to Node.js 22
    
    * fix: replace router.all("*") with router.use() to fix emulator startup issue
    
    * chore: clean up biome analysis file

--- apps/web/src/components/ui/table.tsx
+++ apps/web/src/components/ui/table.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 
 import { cn } from "~/lib/utils";
 
-/* eslint-disable react/prop-types */
-
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
