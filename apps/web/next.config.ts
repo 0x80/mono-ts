a703f1ddf21a840c7de998cddc015e131a477ab9
Thijs Koerselman <thijskoerselman@gmail.com>
2025-08-23 12:06:20 +0200

    Skip nextjs linting

---
+++ apps/web/next.config.ts
@@ -0,0 +1,7 @@
+export default {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
